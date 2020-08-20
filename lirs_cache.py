3aae25d4fb0e2a1ca67741aa14fdcffa3435378f
Robert Fabbro <rfabbro001@gmail.com>
2020-08-20 20:12:14 +0200

    Current LIRS Implementation

--- lirs_cache.py
+++ lirs_cache.py
@@ -4,8 +4,8 @@ import codecs
 
 #Two Stacks: S and Q
 def find_LIR(S: OrderedDict, pg: deque):
-    while S[-1][2]:
-        pg[S[-1]][3] = False
+    while list(S.items())[0][2]:
+        pg[list(S.items())[0]][3] = False
         S.popitem(last=False)
     return S
 
@@ -50,53 +50,102 @@ pg_table = deque()
 
 #[Block Number, is_resident, is_hir_block, in_stack]
 vm_size = get_range(trace)
-for x in range(vm_size):
+for x in range(vm_size+1):
     pg_table.append([x, False, True, False])
 
 PG_HITS = 0
 PG_FAULTS = 0
 free_mem = MAX_MEMORY
+print(pg_table)
+lir_stack[1] = pg_table[1]
+print(lir_stack)
+# lir_stack[1] = 4
+# lir_stack[2] = 7
+# lir_stack[3] = 8
+# print(list(lir_stack.items())[-1])
+# lir_stack.move_to_end(1)
+# lir_stack.popitem(last=True)
+# print(lir_stack)
 
 for i in range(len(trace)):
     ref_block = trace[i]
-    if not pg_table[i][1]:
-        PG_FAULTS += 1
-
-        if free_mem == 0:
-            #pg_table[hir_stack[-1]][1] = False
-            set_residence(pg_table, list(hir_stack.items())[-1][1][0], False)
+    if pg_table[ref_block][1]:
+        PG_HITS += 1
+        #if not a HIR resident and is in stack S
+        if not pg_table[ref_block][2] and pg_table[3]:
+            lir_stack.move_to_end(ref_block)
+            find_LIR(lir_stack, pg_table)
+        elif pg_table[ref_block][2] and pg_table[3]:
             hir_stack.popitem(last=False)
+            set_recency(pg_table, list(lir_stack.keys())[0], False)
+            set_hir_block(pg_table, list(lir_stack.keys())[0], True)
+            hir_stack[list(lir_stack.keys())[0]] = pg_table[list(lir_stack.keys())[0]]
+            lir_stack.move_to_end(list(lir_stack.keys())[0])
+            lir_stack.popitem(last=True)
+
+            lir_stack.move_to_end(ref_block)
+            set_hir_block(pg_table, ref_block, False)
+        elif pg_table[ref_block][2] and not pg_table[ref_block][3]:
+            hir_stack.move_to_end(ref_block)
+    else:
+        PG_FAULTS += 1
+        if free_mem == 0:
+            set_residence(pg_table, list(hir_stack.items())[0], False)
+            hir_stack.popitem(last=False)
+            if pg_table[ref_block][3] and pg_table[2]:
+                set_recency(pg_table, list(lir_stack.keys())[0], False)
+                set_hir_block(pg_table, list(lir_stack.keys())[0], True)
+                hir_stack[list(lir_stack.keys())[0]] = pg_table[list(lir_stack.keys())[0]]
+                lir_stack.move_to_end(list(lir_stack.keys())[0])
+                lir_stack.popitem(last=True)
+
+                set_hir_block(pg_table, ref_block, False)
+                lir_stack[ref_block] = pg_table[ref_block]
+                find_LIR(lir_stack, pg_table)
+            elif not pg_table[ref_block][3] and pg_table[2]:
+                hir_stack[ref_block] = pg_table[ref_block]
             free_mem += 1
         elif free_mem > HIR_SIZE:
             set_hir_block(pg_table, ref_block, False)
-        free_mem -= 1
-    elif pg_table[ref_block][2] and not pg_table[ref_block][3]:
-        hir_stack.pop(ref_block)
-        hir_stack.move_to_end(ref_block)
-        PG_HITS += 1
-    else:
-        PG_HITS += 1
-        lir_stack.move_to_end(ref_block)
+            set_residence(pg_table, ref_block, True)
+            lir_stack[ref_block] = pg_table[ref_block]
+            free_mem -= 1
 
-    lir_stack[ref_block] = pg_table[ref_block]
-    lir_stack.move_to_end(ref_block)
-    set_residence(pg_table, ref_block, True)
 
-    if not pg_table[ref_block][3]:
-        a = 1
-
-    if pg_table[ref_block][2] and pg_table[ref_block][3]:
-        set_hir_block(pg_table, ref_block, False)
-
-    if len(lir_stack) > MAX_MEMORY - HIR_SIZE:
-        set_hir_block(pg_table, ref_block, True)
-        set_recency(pg_table, ref_block, False)
-        hir_stack[ref_block] = pg_table[ref_block]
-        find_LIR(lir_stack, pg_table)
-    elif pg_table[ref_block][3]:
-        hir_stack[ref_block] = pg_table[ref_block]
-
-    pg_table[ref_block][3] = True
+    # if not pg_table[i][1]:
+    #     PG_FAULTS += 1
+    #     if free_mem == 0:
+    #         set_residence(pg_table, list(hir_stack.items())[-1][0], False)
+    #         hir_stack.popitem(last=False)
+    #         #Doesnt add any block here.
+    #         free_mem += 1
+    #     elif free_mem > HIR_SIZE:
+    #         set_hir_block(pg_table, ref_block, False)
+    #     free_mem -= 1
+    #
+    # if pg_table[ref_block][2] and not pg_table[ref_block][3]:
+    #     hir_stack.move_to_end(ref_block)
+    #     free_mem -= 1
+    #     PG_HITS += 1
+    # elif pg_table[ref_block][2] and pg_table[ref_block][3]:
+    #     PG_HITS += 1
+    #     free_mem -= 1
+    #     lir_stack.move_to_end(ref_block)
+    #     set_hir_block(pg_table, ref_block, False)
+    #     set_recency(pg_table, ref_block, True)
+    #     lir_stack[ref_block] = pg_table[ref_block]
+    #     hir_stack.popitem(ref_block)
+    # elif not pg_table[ref_block][2] and pg_table[ref_block][3]:
+    #     PG_FAULTS += 1
+    #     set_hir_block(pg_table, ref_block, False)
+    #     hir_stack[list(lir_stack.keys)[-1]] = pg_table[list(lir_stack.keys)[-1]]
+    #     lir_stack.popitem(last=False)
+    #     find_LIR(lir_stack, pg_table)
+    #
+    #
+    # lir_stack[ref_block] = pg_table[ref_block]
+    # lir_stack.move_to_end(ref_block)
+    # set_residence(pg_table, ref_block, True)
 
 print(PG_HITS)
 print(PG_FAULTS)
