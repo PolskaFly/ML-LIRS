77bad2c728e8d4b69197f9705c39a1bc1a73e90c
Robert Fabbro <rfabbro001@gmail.com>
2020-08-26 15:31:31 -0500

    Re-program of LIRS program

--- lirs_cache.py
+++ lirs_cache.py
@@ -2,21 +2,18 @@ from collections import deque
 from collections import OrderedDict
 import codecs
 
-#Two Stacks: S and Q
-def find_LIR(S: OrderedDict, pg: deque):
-    while list(S.items())[0][1][2]:
-        pg[list(S.items())[0][0]][3] = False
-        S.popitem(last=False)
-    return S
+def find_lru(s: OrderedDict, q: OrderedDict, pg_tbl: deque):
+    temp_s = list(s.keys())
+    while s[temp_s[0]][2]:
+        pg_tbl[temp_s[0]][3] = False
+        try:
+            if q[temp_s[0]]:
+                q[temp_s[0]] = pg_tbl[temp_s[0]]
+        except KeyError:
+            pass
 
-def set_hir_block(pg_table, ref_block, bool):
-    pg_table[ref_block][2] = bool
-
-def set_residence(pg_table, ref_block, bool):
-    pg_table[ref_block][1] = bool
-
-def set_recency(pg_table, ref_block, bool):
-    pg_table[ref_block][3] = bool
+        s.popitem(last=False)
+        del temp_s[0]
 
 def get_range(trace) -> int:
     min = 0
@@ -30,30 +27,33 @@ def get_range(trace) -> int:
 
     return max
 
-#Read File In
+
+# Read File In
 trace = []
-with codecs.open("/Users/polskafly/Desktop/REU/LIRS/traces/sprite.trc", "r", "UTF8") as inputFile:
+with codecs.open("/Users/polskafly/Desktop/REU/LIRS/traces/2_pools.trc", "r", "UTF8") as inputFile:
     inputFile = inputFile.readlines()
 for line in inputFile:
-    trace.append(int(line))
+    if not line == "*\n":
+        trace.append(int(line))
 
-#Initialization Parameters
-MAX_MEMORY = 1000
+# Init Parameters
+MAX_MEMORY = 400
 HIR_PERCENTAGE = 1.0
 MIN_HIR_MEMORY = 2
 
-HIR_SIZE = MAX_MEMORY * (HIR_PERCENTAGE/(100))
+HIR_SIZE = MAX_MEMORY * (HIR_PERCENTAGE / 100)
 if HIR_SIZE < MIN_HIR_MEMORY:
     HIR_SIZE = MIN_HIR_MEMORY
-#Init End
+# Init End
 
 lir_stack = OrderedDict()
 hir_stack = OrderedDict()
 pg_table = deque()
+eviction_list = []
 
-#[Block Number, is_resident, is_hir_block, in_stack]
+# [Block Number, is_resident, is_hir_block, in_stack]
 vm_size = get_range(trace)
-for x in range(vm_size+1):
+for x in range(vm_size + 1):
     pg_table.append([x, False, True, False])
 
 PG_HITS = 0
@@ -61,80 +61,67 @@ PG_FAULTS = 0
 free_mem = MAX_MEMORY
 lir_size = 0
 
-#Possible big issue. I don't move HIR stack objects into LIR stack.
-
 for i in range(len(trace)):
     ref_block = trace[i]
-    if pg_table[ref_block][1]:
-        PG_HITS += 1
-        #if not a HIR resident and is in stack S
-        if not pg_table[ref_block][2] and pg_table[ref_block][3]:
-            lir_stack.move_to_end(ref_block)
-        elif pg_table[ref_block][2] and pg_table[ref_block][3]:
-            lir_stack.move_to_end(ref_block)
-            set_hir_block(pg_table, ref_block, False)
-            lir_stack[ref_block] = pg_table[ref_block]
-            if ref_block in hir_stack:
-                hir_stack.move_to_end(ref_block)
-                hir_stack.popitem(last=True)
-            #Check if HIR is full before popping if I don't get same results as prof.
-            if len(hir_stack) == HIR_SIZE:
-                temp = hir_stack.popitem(last=False)
-                print(i, " ", temp)
-            set_recency(pg_table, list(lir_stack.keys())[0], False)
-            set_hir_block(pg_table, list(lir_stack.keys())[0], True)
-            hir_stack[list(lir_stack.keys())[0]] = pg_table[list(lir_stack.keys())[0]]
-            lir_stack.move_to_end(list(lir_stack.keys())[0])
-            lir_stack.popitem(last=True)
 
-        elif pg_table[ref_block][2] and not pg_table[ref_block][3]:
-            hir_stack.move_to_end(ref_block)
-            hir_stack[ref_block] = pg_table[ref_block]
-        find_LIR(lir_stack, pg_table)
-    else:
+    if not pg_table[ref_block][1]:
         PG_FAULTS += 1
-        lir_stack[ref_block] = pg_table[ref_block]
-        lir_size += 1
         if free_mem == 0:
-            set_residence(pg_table, list(hir_stack.keys())[0], False)
-            temp = hir_stack.popitem(last=False)
-            print(i, " ", temp)
-            if pg_table[ref_block][3] and pg_table[ref_block][2]:
-                set_recency(pg_table, list(lir_stack.keys())[0], False)
-                set_hir_block(pg_table, list(lir_stack.keys())[0], True)
-                hir_stack[list(lir_stack.keys())[0]] = pg_table[list(lir_stack.keys())[0]]
-                lir_stack.move_to_end(list(lir_stack.keys())[0])
-                lir_stack.popitem(last=True)
-
-                set_residence(pg_table, ref_block, True)
-                set_hir_block(pg_table, ref_block, False)
-                lir_stack[ref_block] = pg_table[ref_block]
-                lir_size -= 1
-            elif not pg_table[ref_block][3] and pg_table[ref_block][2]:
-                #Problem
-                set_residence(pg_table, ref_block, True)
-                set_recency(pg_table, ref_block, True)
-                lir_size -= 1
-                lir_stack[ref_block] = pg_table[ref_block]
-                hir_stack[ref_block] = pg_table[ref_block]
+            temp_hir = list(hir_stack)
+            pg_table[temp_hir[0]][1] = False
+            eviction_list.append(hir_stack[temp_hir[0]][0])
+            hir_stack.popitem(last=False)
+            try:
+                lir_stack[temp_hir[0]] = pg_table[temp_hir[0]]
+            except KeyError:
+                pass
             free_mem += 1
         elif free_mem > HIR_SIZE:
-            set_hir_block(pg_table, ref_block, False)
-            set_recency(pg_table, ref_block, True)
-            set_residence(pg_table, ref_block, True)
-            lir_stack[ref_block] = pg_table[ref_block]
-        elif lir_size > MAX_MEMORY - HIR_SIZE:
-            set_recency(pg_table, ref_block, False)
-            set_hir_block(pg_table, ref_block, True)
-            set_residence(pg_table, ref_block, True)
-            hir_stack[ref_block] = pg_table[ref_block]
-            lir_stack[ref_block] = pg_table[ref_block]
-            lir_size -= 1
-
-
+            pg_table[ref_block][2] = False
+            lir_size += 1
         free_mem -= 1
-        find_LIR(lir_stack, pg_table)
+    elif pg_table[ref_block][2]:
+        try:
+            del hir_stack[ref_block]
+        except KeyError:
+            pass
+
+    try:
+        del lir_stack[ref_block]
+    except KeyError:
+        pass
+
+    pg_table[ref_block][1] = True
+    lir_stack[ref_block] = pg_table[ref_block]
+
+    if pg_table[ref_block][2] and pg_table[ref_block][3]:
+        pg_table[ref_block][2] = False
+        lir_stack[ref_block] = pg_table[ref_block]
+        lir_size += 1
+#bug happens here
+        if lir_size > MAX_MEMORY - HIR_SIZE:
+            temp_block = list(lir_stack.keys())[0]
+            pg_table[temp_block][2] = True
+            pg_table[temp_block][3] = False
+            hir_stack[temp_block] = pg_table[temp_block]
+            lir_stack[temp_block] = pg_table[temp_block]
+            find_lru(lir_stack, hir_stack, pg_table)
+            lir_size -= 1
+    elif pg_table[ref_block][2]:
+        hir_stack[ref_block] = pg_table[ref_block]
+
+    pg_table[ref_block][3] = True
+    lir_stack[ref_block] = pg_table[ref_block]
+
+
 
 print(PG_HITS)
 print(PG_FAULTS)
-print(PG_FAULTS+PG_HITS)
+print(PG_FAULTS + PG_HITS)
+
+f = open("evictions.txt", "w")
+for i in range(len(eviction_list)):
+    f.write(str(eviction_list[i]) + "\n")
+f.close()
+
+
