6352af0cd66b18a3c9b842b7caf182596628b478
Robert Fabbro <rfabbro001@gmail.com>
2020-09-01 21:24:41 -0500

    Continued Work.

--- ml_lirs_cache.py
+++ ml_lirs_cache.py
@@ -12,7 +12,7 @@ class LogisticRegression(torch.nn.Module):
         self.linear = torch.nn.Linear(input_dim, output_dim)
 
     def forward(self, x):
-        out = self.linear(x.float())
+        out = self.linear(x)
         return out
 
 def find_lru(s: OrderedDict, pg_tbl: deque):
@@ -30,40 +30,166 @@ def get_range(trace) -> int:
             max = x
     return max
 
-def train_model(features, labels):
+def train_model(features, labels, size, rate):
     # Model Training
-    training_features = torch.from_numpy(np.array(features)).float()
-    training_labels = torch.from_numpy(np.array(labels)).float()
+    features = torch.from_numpy(np.array(features)).type(torch.FloatTensor)
+    labels = torch.from_numpy(np.array(labels)).type(torch.FloatTensor)
 
-    train_ds = TensorDataset(training_features, training_labels)
+    train_ds = TensorDataset(features, labels)
 
-    batch_size = 30
+    batch_size = size
     train_dl = DataLoader(train_ds, batch_size, shuffle=True)
     next(iter(train_dl))
 
     model = LogisticRegression(1, 1)
-    criterion = nn.BCELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=.0000001)
+    criterion = nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=rate)
 
-    for epoch in range(len(training_features)):
+    # m = nn.Sigmoid()
+    # training_features = m(training_features)
+
+    for epoch in range(len(features)):
         for i in enumerate(train_dl):
             # Clear gradients w.r.t. parameters
             optimizer.zero_grad()
 
             # Forward pass to get output/logits
-            outputs = model(training_features.float())
+            outputs = model(features)
 
             # Calculate Loss: softmax --> cross entropy loss
-            loss = criterion(outputs.float(), training_labels.float())
+            loss = criterion(outputs, labels)
 
             # Getting gradients w.r.t. parameters
             loss.backward()
 
             # Updating parameters
             optimizer.step()
-    print(loss)
     return model
 
+
+def LIRS(pg_hits, pg_faults, free_mem, lir_size, trained):
+    training_flt_cntr = 0
+    training_hit_cntr = 0
+    last_ref_block = -1
+    non_zero = False
+    inf_count = 0
+    for i in range(len(trace)):
+        if non_zero:
+            if training_hit_cntr + training_flt_cntr > TRAINING_DATA_MIN and not trained:
+                m = train_model(training_data, label_data, 30, .01)
+                training_data.clear()
+                label_data.clear()
+                training_flt_cntr = 0
+                training_hit_cntr = 0
+                trained = True
+            elif training_hit_cntr + training_flt_cntr > 30 and trained:
+                m = train_model(training_data, label_data, 10, .1)
+                training_data.clear()
+                label_data.clear()
+                training_flt_cntr = 0
+                training_hit_cntr = 0
+
+        ref_block = trace[i]
+
+        if ref_block == last_ref_block:
+            pg_hits += 1
+            continue
+        else:
+            pass
+
+        if not pg_table[ref_block][1]:
+            pg_faults += 1
+            if free_mem == 0:
+                temp_hir = list(hir_stack)
+                pg_table[temp_hir[0]][1] = False
+
+                # Gathering and labeling training data (always happening)
+                if i > DATA_COLLECTION_START:
+                    if pg_table[temp_hir[0]][4] == 9999 and inf_count < 50:
+                        training_data.append([pg_table[temp_hir[0]][4]])
+                        label_data.append([0])
+                        training_flt_cntr += 1
+                        non_zero = True
+                        inf_count += 1
+                    elif pg_table[temp_hir[0]][4] != 9999 and inf_count >= 50:
+                        training_data.append([pg_table[temp_hir[0]][4]])
+                        label_data.append([0])
+                        training_flt_cntr += 1
+                        non_zero = True
+
+                eviction_list.append(temp_hir[0])
+                hir_stack.popitem(last=False)
+                free_mem += 1
+            elif free_mem > HIR_SIZE:
+                pg_table[ref_block][2] = False
+                lir_size += 1
+            free_mem -= 1
+        elif pg_table[ref_block][2]:
+            if hir_stack.get(ref_block):
+                del hir_stack[ref_block]
+
+        if pg_table[ref_block][1]:
+            pg_hits += 1
+            if i > DATA_COLLECTION_START:
+                if pg_table[temp_hir[0]][4] == 9999 and inf_count < 50:
+                    training_data.append([pg_table[ref_block][4]])
+                    label_data.append([1])
+                    training_hit_cntr += 1
+                    non_zero = True
+                    inf_count += 1
+                elif pg_table[temp_hir[0]][4] != 9999 and inf_count >= 50:
+                    training_data.append([pg_table[ref_block][4]])
+                    label_data.append([1])
+                    training_hit_cntr += 1
+                    non_zero = True
+
+        if lir_stack.get(ref_block):
+            temp_lir = list(lir_stack)
+            trigger = False
+            counter = 0
+            for x in range(len(temp_lir)):
+                if temp_lir[x] == ref_block:
+                    trigger = True
+                if trigger:
+                    counter += 1
+            pg_table[ref_block][4] = counter
+            del lir_stack[ref_block]
+            find_lru(lir_stack, pg_table)
+
+        pg_table[ref_block][1] = True
+        lir_stack[ref_block] = pg_table[ref_block][0]
+
+        if not trained:
+            if pg_table[ref_block][2] and pg_table[ref_block][3]:
+                pg_table[ref_block][2] = False
+                lir_size += 1
+                if lir_size > MAX_MEMORY - HIR_SIZE:
+                    temp_block = list(lir_stack)[0]
+                    pg_table[temp_block][2] = True
+                    pg_table[temp_block][3] = False
+                    hir_stack[temp_block] = lir_stack[temp_block]
+                    find_lru(lir_stack, pg_table)
+                    lir_size -= 1
+            elif pg_table[ref_block][2]:
+                hir_stack[ref_block] = pg_table[ref_block][0]
+        else:
+            #BIG LOGIC ERROR HERE.
+            if m(torch.tensor([pg_table[ref_block][4]]).type(torch.FloatTensor)) > .5 and pg_table[ref_block][3]:
+                pg_table[ref_block][2] = False
+                lir_size += 1
+                if lir_size > MAX_MEMORY - HIR_SIZE:
+                    temp_block = list(lir_stack)[0]
+                    pg_table[temp_block][2] = True
+                    pg_table[temp_block][3] = False
+                    hir_stack[temp_block] = lir_stack[temp_block]
+                    find_lru(lir_stack, pg_table)
+                    lir_size -= 1
+            else:
+                hir_stack[ref_block] = pg_table[ref_block][0]
+
+        pg_table[ref_block][3] = True
+
+        last_ref_block = ref_block
 
 # Read File In
 trace = []
@@ -83,7 +209,7 @@ if HIR_SIZE < MIN_HIR_MEMORY:
     HIR_SIZE = MIN_HIR_MEMORY
 
 TRAINING_HIT_PERCENTAGE = 30.0
-TRAINING_DATA_MIN = 200
+TRAINING_DATA_MIN = 500
 DATA_COLLECTION_START = MAX_MEMORY * 10
 # Init End
 
@@ -106,117 +232,9 @@ PG_FAULTS = 0
 free_mem = MAX_MEMORY
 lir_size = 0
 in_trace = 0
-last_ref_block = -1
+is_trained = False
 
-#training variables
-training_flt_cntr = 0
-training_hit_cntr = 0
-non_zero = False
-inf_count = 0
-stop_inf = False
-
-
-
-for i in range(len(trace)):
-    if non_zero:
-        if training_hit_cntr/(training_flt_cntr + training_hit_cntr)*100 >= TRAINING_HIT_PERCENTAGE and \
-                training_flt_cntr + training_hit_cntr > TRAINING_DATA_MIN:
-            train_model(training_data, label_data)
-            training_data.clear()
-            label_data.clear()
-
-
-    ref_block = trace[i]
-
-    if ref_block == last_ref_block:
-        PG_HITS += 1
-        continue
-    else:
-        pass
-
-    in_trace += 1
-    if not pg_table[ref_block][1]:
-        PG_FAULTS += 1
-        if free_mem == 0:
-            temp_hir = list(hir_stack)
-            pg_table[temp_hir[0]][1] = False
-
-            #Gathering and labeling training data (always happening)
-            if i > DATA_COLLECTION_START:
-                if pg_table[temp_hir[0]][4] == 9999 and inf_count < 50:
-                    training_data.append([pg_table[temp_hir[0]][4]])
-                    label_data.append([False])
-                    training_flt_cntr += 1
-                    non_zero = True
-                    inf_count += 1
-                elif pg_table[temp_hir[0]][4] != 9999 and inf_count >= 50:
-                    training_data.append([pg_table[temp_hir[0]][4]])
-                    label_data.append([False])
-                    training_flt_cntr += 1
-                    non_zero = True
-
-            eviction_list.append(temp_hir[0])
-            hir_stack.popitem(last=False)
-            free_mem += 1
-        elif free_mem > HIR_SIZE:
-            pg_table[ref_block][2] = False
-            lir_size += 1
-        free_mem -= 1
-    elif pg_table[ref_block][2]:
-        if hir_stack.get(ref_block):
-            del hir_stack[ref_block]
-
-    if pg_table[ref_block][1]:
-        PG_HITS += 1
-        if i > DATA_COLLECTION_START:
-            if pg_table[temp_hir[0]][4] == 9999 and inf_count < 50:
-                training_data.append([pg_table[ref_block][4]])
-                label_data.append([True])
-                training_hit_cntr += 1
-                non_zero = True
-                inf_count += 1
-            elif pg_table[temp_hir[0]][4] != 9999 and inf_count >= 50:
-                training_data.append([pg_table[ref_block][4]])
-                label_data.append([True])
-                training_hit_cntr += 1
-                non_zero = True
-
-
-    if lir_stack.get(ref_block):
-        temp_lir = list(lir_stack)
-        trigger = False
-        counter = 0
-        for x in range(len(temp_lir)):
-            if temp_lir[x] == ref_block:
-                trigger = True
-            if trigger:
-                counter += 1
-        pg_table[ref_block][4] = counter
-        del lir_stack[ref_block]
-        find_lru(lir_stack, pg_table)
-
-    pg_table[ref_block][1] = True
-    lir_stack[ref_block] = pg_table[ref_block][0]
-
-
-    if pg_table[ref_block][2] and pg_table[ref_block][3]:
-        pg_table[ref_block][2] = False
-        lir_size += 1
-        if lir_size > MAX_MEMORY - HIR_SIZE:
-            temp_block = list(lir_stack)[0]
-            pg_table[temp_block][2] = True
-            pg_table[temp_block][3] = False
-            hir_stack[temp_block] = lir_stack[temp_block]
-            find_lru(lir_stack, pg_table)
-            lir_size -= 1
-    elif pg_table[ref_block][2]:
-        hir_stack[ref_block] = pg_table[ref_block][0]
-
-    pg_table[ref_block][3] = True
-
-    last_ref_block = ref_block
-
-
+LIRS(PG_HITS, PG_FAULTS, free_mem, lir_size, is_trained)
 
 
 print("Hits: ", PG_HITS)
