9e58e86fcbb702494da8617a27ec6aaf37fb65b8
Robert Fabbro <rfabbro001@gmail.com>
2020-10-01 16:34:12 -0500

    Added comments.

--- ml_lirs_cache.py
+++ ml_lirs_cache.py
@@ -43,7 +43,7 @@ def print_information(hits, faults, size):
     print("HIR Size: ", size)
     print("Hit Ratio: ", hits / (hits + faults) * 100)
 
-def online_training(model, features, labels, size, rate):
+def logReg_training(model, features, labels, size, rate):
     temp_features = torch.from_numpy(np.array(features)).type(torch.FloatTensor)
     temp_labels = torch.from_numpy(np.array(labels)).type(torch.FloatTensor)
 
@@ -75,9 +75,6 @@ def online_training(model, features, labels, size, rate):
 
     return model
 
-def GBC(model, x_train, y_train, learning_rate):
-    model.fit(x_train, y_train)
-
 def replace_lir_block(pg_table, lir_size):
     temp_block = lir_stack.popitem(last=False)
     pg_table[temp_block[1]][2] = True
@@ -97,32 +94,34 @@ def LIRS(trace, pg_table, block_range_window):
     trained = False
     training_data = deque()
     label_data = deque()
-    train_amount = 1000
+    train_amount = 100
 
+    count_evict = 0
     count_nonevict = 0
 
 
     #initializing model
     # model = LogisticRegression(4, 1)
-    model = GradientBoostingClassifier(n_estimators=25, learning_rate=.1, max_features=4, max_depth=10,
+    model = GradientBoostingClassifier(n_estimators=30, learning_rate=.1, max_features=2, max_depth=15,
                                         random_state=0)
 
     for i in range(len(trace)):
         ref_block = trace[i]
 
+        # setting block range
         if len(block_range_window) == BLOCK_RANGE:
             pg_table[ref_block][5] = min(block_range_window)
             pg_table[ref_block][6] = max(block_range_window)
             pg_table[ref_block][7] = median(block_range_window)
 
+        # maintaining range window
         block_range_window.append(ref_block)
         if len(block_range_window) > BLOCK_RANGE:
             block_range_window.popleft()
 
+        #training
         if len(training_data) > train_amount:
-            # online_training(model, training_data, label_data, 1, .001)
-            # scaler = MinMaxScaler()
-            # training_data = scaler.fit_transform(training_data)
+            # logReg_training(model, training_data, label_data, 1, .001)
             model.fit(training_data, label_data)
             del training_data
             del label_data
@@ -130,7 +129,9 @@ def LIRS(trace, pg_table, block_range_window):
             label_data = []
             trained = True
             count_nonevict = 0
+            count_evict = 0
 
+        #Beginning of standard LIRS logic
         if ref_block == last_ref_block:
             pg_hits += 1
             pg_table[ref_block][4] = 0
@@ -143,11 +144,16 @@ def LIRS(trace, pg_table, block_range_window):
             if free_mem == 0:
                 evicted_hir = hir_stack.popitem(last=False)
                 pg_table[evicted_hir[1]][1] = False
-                training_data.append([pg_table[ref_block][4], pg_table[ref_block][5],
-                                      pg_table[ref_block][6], pg_table[ref_block][7]])
-                label_data.append(-1)
+
+                # gathering training and label data of evicted objects
+                if count_evict <= train_amount/2:
+                    training_data.append([pg_table[ref_block][4], pg_table[ref_block][5],
+                                          pg_table[ref_block][6], pg_table[ref_block][7]])
+                    label_data.append(-1)
+                    count_evict += 1
+
                 free_mem += 1
-            elif free_mem > HIR_SIZE:
+            elif free_mem > HIR_SIZE:  # Exception for when the cache is initially empty and is being filled.
                 pg_table[ref_block][2] = False
                 lir_size += 1
             free_mem -= 1
@@ -161,39 +167,38 @@ def LIRS(trace, pg_table, block_range_window):
 
         if lir_stack.get(ref_block):
             counter = 0
-            for j in lir_stack.keys():  # Reuse distance
+            for j in lir_stack.keys():  # Getting the reuse distance
                 counter += 1
                 if lir_stack[j] == ref_block:
                     break
-            pg_table[ref_block][4] = (len(lir_stack) - counter)
+            pg_table[ref_block][4] = (len(lir_stack) - counter)  # Setting the reuse distance for that block
 
-            if count_nonevict < train_amount/2:
+            # gathering training data and label data of objects in cache
+            if count_nonevict <= train_amount/2:
                 training_data.append([pg_table[ref_block][4], pg_table[ref_block][5],
                                         pg_table[ref_block][6], pg_table[ref_block][7]])
                 label_data.append(1)
                 count_nonevict += 1
+
+
             del lir_stack[ref_block]
             find_lru(lir_stack, pg_table)
 
-        # if len(block_range_window) == BLOCK_RANGE:
-        #     pg_table[ref_block][5] = min(block_range_window)
-        #     pg_table[ref_block][6] = max(block_range_window)
-        #     pg_table[ref_block][7] = median(block_range_window)
+        pg_table[ref_block][1] = True  # Sets the blocks residency in the LIR stack to True.
+        lir_stack[ref_block] = pg_table[ref_block][0]  # Adds the block to the LIR stack.
 
-        pg_table[ref_block][1] = True
-        lir_stack[ref_block] = pg_table[ref_block][0]
-
-        if pg_table[ref_block][2] and pg_table[ref_block][3]:
+        if pg_table[ref_block][2] and pg_table[ref_block][3]:  # Logic for if the block is HIR and is a resident in LIR.
+            # If above is true, it makes the HIR block a LIR block.
             pg_table[ref_block][2] = False
             lir_size += 1
 
-            if lir_size > MAX_MEMORY - HIR_SIZE:
+            if lir_size > MAX_MEMORY - HIR_SIZE:  # If LIR stack is now greater than it's max size, it prunes itself.
                 replace_lir_block(pg_table, lir_size)
 
-        elif pg_table[ref_block][2]:
-            if not trained:
+        elif pg_table[ref_block][2]:  # If it is an HIR block but not in the stack.
+            if not trained:  # Standard LIRS logic.
                 hir_stack[ref_block] = pg_table[ref_block][0]
-            else:
+            else:  # If trained, it now takes advantage of ML model.
                 # prediction = model(torch.sigmoid(torch.tensor([pg_table[ref_block][4], pg_table[ref_block][5],
                 #                                                pg_table[ref_block][6], pg_table[ref_block][7]])
                 #                                  .type(torch.FloatTensor)))
@@ -209,7 +214,7 @@ def LIRS(trace, pg_table, block_range_window):
                 else:
                     hir_stack[ref_block] = pg_table[ref_block][0]
 
-        pg_table[ref_block][3] = True
+        pg_table[ref_block][3] = True  # Sets it's recency in the LIR stack to true.
 
         last_ref_block = ref_block
 
@@ -219,14 +224,14 @@ def LIRS(trace, pg_table, block_range_window):
 
 # Read File In
 trace_array = []
-with codecs.open("/Users/polskafly/Desktop/REU/LIRS/traces/sprite.trc", "r", "UTF8") as inputFile:
+with codecs.open("/Users/polskafly/Desktop/REU/LIRS/traces/ps.trc", "r", "UTF8") as inputFile:
     inputFile = inputFile.readlines()
 for line in inputFile:
-    if not line == "*\n" or not line == "\n":
+    if not line == "*\n" or not line == "\n" or not line == '*\n':
         trace_array.append(int(line))
 
 # Init Parameters
-MAX_MEMORY = 1000
+MAX_MEMORY = 700
 HIR_PERCENTAGE = 1.0
 MIN_HIR_MEMORY = 2
 
